4a71561856997fed30d81a191bda8a288058866a
Maxim Kossov <118305012+Aiszhio@users.noreply.github.com>
2026-04-26 19:34:45 +0300

    add grpc server adapter

---
+++ internal/api/grpc_server.go
@@ -0,0 +1,28 @@
+package api
+
+import (
+	"context"
+
+	"github.com/Aiszhio/StubExplainer/internal/service"
+	explainerv1 "github.com/Aiszhio/StubExplainer/pkg/gen/explainer/v1"
+)
+
+// GRPCServer implements the generated-like ExplainerServiceServer contract.
+type GRPCServer struct {
+	service *service.ExplainerService
+}
+
+func NewGRPCServer(service *service.ExplainerService) *GRPCServer {
+	return &GRPCServer{service: service}
+}
+
+func (s *GRPCServer) GetExplanation(ctx context.Context, req *explainerv1.GetExplanationRequest) (*explainerv1.GetExplanationResponse, error) {
+	item, err := s.service.GetExplanation(req.IncidentId)
+	if err != nil {
+		return nil, err
+	}
+
+	return &explainerv1.GetExplanationResponse{
+		Explanation: ToProto(item),
+	}, nil
+}
